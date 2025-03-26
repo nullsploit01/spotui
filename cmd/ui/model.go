5395fb3536cd4ac00e394943bc5fcc05b419c7f4
nullsploit01 <harshaldharmik099@gmail.com>
2025-03-26 22:34:12 +0530

    add ability to use fullscreen and center search bar

--- cmd/ui/model.go
+++ cmd/ui/model.go
@@ -2,10 +2,12 @@ package ui
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
 	input string
+	width int
 }
 
 func (m model) Init() tea.Cmd {
@@ -14,13 +16,21 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter:
-		case tea.KeyBackspace:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+
+		case "enter":
+
+		case "backspace":
 			if len(m.input) > 0 {
 				m.input = m.input[:len(m.input)-1]
 			}
+
 		default:
 			m.input += msg.String()
 		}
@@ -29,12 +39,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return SearchInput.Render("🔍 " + m.input)
+	content := "🔍 " + m.input
+
+	rendered := inputStyle.Render(content)
+	centered := lipgloss.PlaceHorizontal(m.width, lipgloss.Center, rendered)
+
+	return "\n" + centered
 }
 
 func StartUI() error {
 	m := model{}
-	p := tea.NewProgram(m)
+	p := tea.NewProgram(m, tea.WithAltScreen())
 	_, err := p.Run()
 	return err
 }
--- cmd/ui/styles.go
+++ cmd/ui/styles.go
@@ -4,7 +4,12 @@ import "github.com/charmbracelet/lipgloss"
 
 var (
 	SearchInput = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#00FFAA")).
-		Border(lipgloss.ThickBorder()).
-		Padding(0, 1)
+			Foreground(lipgloss.Color("#00FFAA")).
+			Border(lipgloss.ThickBorder()).
+			Padding(0, 1)
+
+	inputStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#00FFAA")).
+			Border(lipgloss.NormalBorder()).
+			Padding(0, 1)
 )
